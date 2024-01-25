7ac8d64dd14e8267d968f0d947b8b4e8e8524d39
fang <fang@fang>
2024-01-25 07:22:43 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className='flex flex-col h-screen gap-[5vw] bg-black'>
       <div className='filter flex bg-[#333333] text-white blur-[2px] hover:blur-[0px]'>
-        <p>Hello</p>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </div>
 
       <div className='flex bg-[#333333] text-white blur-[2px] hover:blur-[0px]'>
