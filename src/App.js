bd5ee6e764b0e8860143f10d97b68cfbb1b2c49a
MOHAMED ABDULLAH FAHAD <90413824+FangChat@users.noreply.github.com>
2024-01-25 07:06:27 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+
+  return (
+    <div className='flex flex-col h-screen gap-[5vw] bg-black'>
+      <div className='filter flex bg-[#333333] text-white blur-[2px] hover:blur-[0px]'>
+        <p>Hello</p>
+      </div>
+
+      <div className='flex bg-[#333333] text-white blur-[2px] hover:blur-[0px]'>
+        <p>Hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
